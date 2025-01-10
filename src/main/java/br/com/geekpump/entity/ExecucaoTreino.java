290c7bd0387473715ce4ca48f3b187a557359ecd
Evandro Moura <evandro@trixti.com.br>
2025-01-10 02:02:26 -0300

    Modelagem Execucao Treino

---
+++ src/main/java/br/com/geekpump/entity/ExecucaoTreino.java
@@ -0,0 +1,56 @@
+package br.com.geekpump.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "execucao_treino", schema = "public")
+
+public class ExecucaoTreino {
+
+	@Id
+	@SequenceGenerator(name="EXECUCAO_TREINO_ID_GENERATOR", sequenceName="execucao_treino_id_seq", allocationSize = 1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="EXECUCAO_TREINO_ID_GENERATOR")
+	
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name="id_treino_usuario_divisao_exercicio")
+	private TreinoUsuarioDivisaoExercicio treinoUsuarioDivisaoExercicio;
+	
+	@Column(name= "data_execucao")
+	private Date dataExecucao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public TreinoUsuarioDivisaoExercicio getTreinoUsuarioDivisaoExercicio() {
+		return treinoUsuarioDivisaoExercicio;
+	}
+
+	public void setTreinoUsuarioDivisaoExercicio(TreinoUsuarioDivisaoExercicio treinoUsuarioDivisaoExercicio) {
+		this.treinoUsuarioDivisaoExercicio = treinoUsuarioDivisaoExercicio;
+	}
+
+	public Date getDataExecucao() {
+		return dataExecucao;
+	}
+
+	public void setDataExecucao(Date dataExecucao) {
+		this.dataExecucao = dataExecucao;
+	}
+}
---
+++ src/main/java/br/com/geekpump/service/execucaotreino/ExecucaoTreinoService.java
@@ -0,0 +1,19 @@
+package br.com.geekpump.service.execucaotreino;
+
+import javax.inject.Inject;
+
+import br.com.geekpump.dao.AbstractDAO;
+import br.com.geekpump.dao.execucaotreino.ExecucaoTreinoDAO;
+import br.com.geekpump.entity.ExecucaoTreino;
+import br.com.geekpump.service.AbstractService;
+
+public class ExecucaoTreinoService extends AbstractService<ExecucaoTreino> {
+
+	private @Inject ExecucaoTreinoDAO execucaoTreinoDAO;
+	
+	@Override
+	public AbstractDAO<ExecucaoTreino> getDAO() {
+		return execucaoTreinoDAO;
+	}
+
+}
